3181a1033c01283164938789f08237c783eda1aa
Glenn Moore <glenn.moore@gibraltar-financial.com>
2020-12-12 15:27:55 -0500

    Saturday 12.12.2020

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,24 +4,45 @@ import csv
 #specify file to pull from:
 budget_data = os.path.join("Resources", "budget_data.csv")
 
+#Name Lists
+Month=[]
+Profit_Losses=[]
+Avg_Change=[]
+
 #Open CSV
 with open(budget_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        Month.append(row[0]) 
+        Profit_Losses.append(int(row[1]))
 
 #Calculate the total number of months included in the dataset
-total_months = budget_data.count() 
-
-print(total_months)
-
+    total_months = len(Month)
 
 #Calculate the net total amount of "Profit/Losses" over the entire period
-
-
+    total_profit_losses=0
+    for row in Profit_Losses:
+        total_profit_losses = int(total_profit_losses) + int(row)
 
 #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+for r in range(len(Profit_Losses)-1):
+    Avg_Change.append(Profit_Losses[r+1]-Profit_Losses[r])
 
+def mean(changes):
+    return sum(changes) / len(changes)
 
 #Calculate the greatest increase in profits (date and amount) over the entire period
+Max_Profit = max(Profit_Losses)
+Max_Date = str(Month[Avg_Change.index(max(Avg_Change))+1])
 
 
 #Calculate the greatest decrease in losses (date and amount) over the entire period
+Min_Profit = min(Profit_Losses)
+Min_Date = str(Month[Avg_Change.index(min(Avg_Change))+1])
+
+print(f'The total number of months included in the dataset is {len(Month)}')
+print(f'The net total amount of Profit & Losses over the entire preiod is $ {sum(Profit_Losses)}')
+print(f'The average change is $ {round(sum(Avg_Change)/len(Avg_Change),2)}')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,32 +6,57 @@ poll_data = os.path.join("Resources", "election_data.csv")
 #Variables 
 votes = []
 candidates = []
+Kahn_count = int()
+Correy_count = int()
+Li_count = int()
+OTooley_count = int()
+results = {}
 
 #Read CSV File
-with open(poll_data,'r') as csvfile:
+with open(poll_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    print(csvreader)
+    csv_header = next(csvfile)
+
+    next(csvreader)
     
 
-#Calculate the total number of votes cast
-total_votes = count("Voter ID")
+# #Calculate the total number of votes cast
+# total_votes = 0
 
-#Calculate a complete list of candidates who received votes
-
-
-#Calculate the total number of votes each candidate won
-Kahn = int(Candidate.count("Kahn"))
-Correy = int(Candidate.count("Correy"))
-Li = int(Candidate.count("Li"))
-O'Tooley = int(Candidate.count("O'Tooley"))
-
-#Calculate the percentage of votes each candidate won
-percentage_Kahn = Kahn/total_votes
-percentage_Correy = Correy/total_votes
-percentage_Li = Li/total_votes
-percentage_O'Tooley = O'Tooley/total_votes
+# #Calculate a complete list of candidates who received votes
+candidate_list = ["Kahn", "Correy", "Li", "O'Toole"]
 
 
+# #Calculate the total number of votes each candidate won
+# Kahn = int(Candidate.count("Kahn"))
+# Correy = int(Candidate.count("Correy"))
+# Li = int(Candidate.count("Li"))
+# O'Tooley = int(Candidate.count("O'Tooley"))
+
+# #Calculate the percentage of votes each candidate won
+# percentage_Kahn = Kahn/total_votes
+# percentage_Correy = Correy/total_votes
+# percentage_Li = Li/total_votes
+# percentage_O'Tooley = O'Tooley/total_votes
 
 
-#Calculate the winner of the election based on popular vote.
+
+
+# #Calculate the winner of the election based on popular vote.
+# #define output path
+# output_file = os.path.join("analysis.txt")
+print('Election Results')
+print('----------------------------------------')
+print(f'Total Votes: {count}')
+    for canditate in candidates:
+        print(f'{candidate}: {percentages[candidate]}% ({votes[candidate]}')
+print(f'Winner: {winner')
+
+#Write text to file
+with open(textpath,'w') as textfile:
+   textfile.write('Election Results\n')
+   textfile.write('----------------------------------------')
+   textfile.write(f'Total Votes: {count}\n')
+
+
+   textfile.write(f'Winner: {winner}')
